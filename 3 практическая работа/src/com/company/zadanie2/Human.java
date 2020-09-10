c2f63d3d422c5e0dc9c8d2dd110fcd7659f41df9
Merfarion <64864827+Merfarion@users.noreply.github.com>
2020-09-10 11:50:24 +0300

    Add files via upload

--- 3 практическая работа/src/com/company/zadanie2/Human.java
+++ 3 практическая работа/src/com/company/zadanie2/Human.java
@@ -29,4 +29,46 @@ public class Human {
         leg.comment();
         head.intelligence();
     }
+    public class Hand {
+        private int fingers;
+        public Hand(int fingers){
+            this.fingers=fingers;
+        }
+        public void status (){
+            if (fingers == 5){
+                System.out.println("Нормальная рука");
+            }
+            else {
+                System.out.println("А кому нужны пальцы?");
+            }
+        }
+    }
+    public class Head {
+        private   String form;
+        public Head(String form){
+            this.form = form;
+        }
+        public void intelligence() {
+            System.out.println("Интеллект отсутствует");
+        }
+    }
+    public class Leg {
+        private int size;
+        public Leg (int size){
+            this.size = size;
+        }
+        public void comment(){
+            if(size <39) {
+                System.out.println("Ножка Золушки");
+            }
+            else
+            if(size<43){
+
+                System.out.println("Нормальный размер ноги");
+            }
+            else{
+                System.out.println("Лыжи");
+            }
+        }
+    }
 }
