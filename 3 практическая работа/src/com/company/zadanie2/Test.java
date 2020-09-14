c8b2ccf81271ae54567d553a81083b4791d6b5a5
Merfarion <64864827+Merfarion@users.noreply.github.com>
2020-09-14 14:23:11 +0300

    Update Test.java

--- 3 практическая работа/src/com/company/zadanie2/Test.java
+++ 3 практическая работа/src/com/company/zadanie2/Test.java
@@ -2,9 +2,10 @@ package com.company.zadanie2;
 
 public class Test {
     public static void main(String[] args){
-Human gleb = new Human("Gleb",184,62.5);
+Human gleb = new Human("Gleb",184,62.5, 5,5,43,43,"round");
+gleb.setHead("oval");
 System.out.println(gleb.toString());
-gleb.vyvod();
+
     }
 
 
