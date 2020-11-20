846d3cc0ccbac825db24f7799b50415a29d77074
gleba <glebarian@gmail.com>
2020-11-20 18:01:45 +0300

    Работаем

--- 21-22 практическая работа/src/com/company/Item.java
+++ 21-22 практическая работа/src/com/company/Item.java
@@ -1,4 +1,57 @@
 package com.company;
 
 public class Item {
+    private int id;
+    private String data;
+    private boolean flag;
+    private int someNumber;
+
+    public Item(int id, String data, boolean flag, int someNumber) {
+        this.id = id;
+        this.data = data;
+        this.flag = flag;
+        this.someNumber = someNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public boolean isFlag() {
+        return flag;
+    }
+
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
+
+    public int getSomeNumber() {
+        return someNumber;
+    }
+
+    public void setSomeNumber(int someNumber) {
+        this.someNumber = someNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", data='" + data + '\'' +
+                ", flag=" + flag +
+                ", someNumber=" + someNumber +
+                '}';
+    }
 }
---
+++ 21-22 практическая работа/src/com/company/JsonSample.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class JsonSample {
+    public static void main(String[] args) {
+        Item item = new Item(10, "Ball", true, 45);
+        System.out.println(item);
+    }
+}
